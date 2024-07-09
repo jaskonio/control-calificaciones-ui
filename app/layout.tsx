8d2a2e752fe4ff5d6a4fc39056c31d09e29c08fa
DESKTOP-2UVI5A0\jonatan.lopez <joalopez@eservicios.indra.es>
2024-07-09 13:51:40 +0200

    NextTopLoader added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import NextTopLoader from 'nextjs-toploader';
+
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={outfit.className}>{children}</body>
+    <html lang="es">
+      <body className={outfit.className}>
+        <NextTopLoader color="#000"/>
+        {children}
+      </body>
     </html>
   );
 }
